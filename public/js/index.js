4f3a6839bf0d3450b1cf9984f48ec7ad8a9d63f4
Cees <cjol99@icloud.com>
2020-03-16 13:06:34 +0100

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,35 @@
 var socket = io();
 
-socket.on('connect', () => {
-    console.log('Connected to server');
+socket.on('connect', function() {
+  console.log('Connected to server');
 });
 
-socket.on('disconnect', () => {
-    console.log('Disconnected to server');
+socket.on('disconnect', function() {
+  console.log('Disconnected to server');
 });
 
 socket.on('newMessage', function (message) {
-    console.log(message);
+  console.log(message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+socket.emit('createMessage', {
+  from: 'Frank',
+  text: 'hi'
+}, function(data) {
+  console.log('Got it', data)
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -4,13 +4,11 @@ var {generateMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
-    var from = 'Cees';
-    var text = 'hey';
+    var from = 'Jen';
+    var text = 'Some message';
     var message = generateMessage(from, text);
 
-    expect(message.from).toEqual('Cees');
-    expect(message.text).toEqual('hey');
-    expect(message).toInclude({from, text});
     expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
   });
 });
