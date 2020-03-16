567cc491e691b4b6be514200395dce933a7c7084
Cees <cjol99@icloud.com>
2020-03-16 14:16:24 +0100

    Format timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -2,16 +2,20 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
+
+  setStatus('Online');
 });
 
 socket.on('disconnect', function() {
   console.log('Disconnected to server');
+
+  setStatus('Offline');
 });
 
 socket.on('newMessage', function (message) {
-  console.log(message);
+  var formattedTime = moment(message.createdAt).format('HH:mm');
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
@@ -23,13 +27,45 @@ socket.emit('createMessage', {
   console.log('Got it', data)
 });
 
+var sendButton = jQuery('#send-button');
+sendButton.attr('disabled', 'disabled');
+var messageTextbox = jQuery('[name=message]');
+var sendEnabled = false;
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  disableSendButton();
+
+  setStatus('Sending message...');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function() {
-
+    messageTextbox.val('');
+    setStatus('Online');
   });
 });
+
+// Detect input changes in textfield, and set send-button to disabled or not
+messageTextbox.on('input', function(e) {
+  if (sendEnabled && messageTextbox.val() == '') {
+    disableSendButton();
+  } else if (!sendEnabled && messageTextbox.val() !== '') {
+    enableSendButton();
+  }
+});
+
+function disableSendButton() {
+  sendButton.attr('disabled', 'disabled');
+  sendEnabled = false;
+}
+
+function enableSendButton() {
+  sendButton.removeAttr('disabled');
+  sendEnabled = true;
+}
+
+function setStatus(x) {
+  jQuery('#status').text(x);
+}
