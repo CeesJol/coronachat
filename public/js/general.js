7958879dd382f4389fc5e8051532b7154551d7c1
Cees <cjol99@icloud.com>
2020-03-18 23:13:05 +0100

    Refactor

--- public/js/general.js
+++ public/js/general.js
@@ -129,3 +129,18 @@ function setButton(x) {
 function isRealString(str) {
   return typeof str === 'string' && str.trim().length > 0;
 }
+
+/**
+ * Set vh
+ */
+function myFunction() {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  console.log(vh);
+} myFunction();
+// We listen to the resize event
+window.addEventListener('resize', function() {
+  myFunction();
+});
