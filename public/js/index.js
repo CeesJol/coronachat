47feff7fb3776de17c7aa223f5a1fd0e3f1c1550
Cees <cjol99@icloud.com>
2020-03-21 13:56:31 +0100

    Add optional/developer sound if someone joins the landing page

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,7 @@ socket.on('responseUserAmount', function(data) {
       result = '<p style="color: red"><b>' + data + appendix + '</b></p>';
     } else {
       result = '<p style="color: green"><b>' + data + appendix + '</b></p>';
+      playSound();
     }
 
     prevData = data;
@@ -23,3 +24,13 @@ socket.on('responseUserAmount', function(data) {
     document.getElementById('status').innerHTML = result;
   }
 });
+
+// Play a sound if someone joins, only if you're a developer.
+var developer = false;
+function playSound() {
+  if (developer) {
+    const AUDIO_LOCATION = '/./audio/25879__acclivity__drip1.wav';
+    var audio = new Audio(AUDIO_LOCATION);
+    audio.play();  
+  }
+}
