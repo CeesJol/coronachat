b3a5f6338d4ac335e85e9d6c4bd0b83b66b042ad
Cees <cjol99@icloud.com>
2020-03-29 11:09:44 +0200

    Add translation method and Dutch translations for index page

---
+++ public/js/index-language.js
@@ -0,0 +1,21 @@
+var myLanguage = navigator.language || navigator.userLanguage;
+
+var languages = {
+  'nl-NL': {
+    'subtitle': 'Anoniem chatten',
+    'join-a-chat': 'Begin met chatten',
+    'slogan': 'Begin een gesprek met andere mensen die afstand moeten houden, volledig anoniem.', 
+    'chat-name-label': 'Naam',
+    'chat-name': 'Chat naam',
+    'join-button': 'Meedoen',
+    'rooms-label': 'Kamers'
+  }
+}
+
+if (languages[myLanguage]) {
+  for (const property in languages[myLanguage]) {
+    document.getElementById(property).innerHTML = languages[myLanguage][property];
+  }
+}
+
+console.log("Your language is " + myLanguage);
